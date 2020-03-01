2c6e410f25368aac50a3ad1be4c68cace18da74e
ahmedmeshref <a.meshref@alustudent.com>
2020-03-02 01:03:47 +0200

    re interview questions' solutions added

--- subarray_sum/solution.py
+++ subarray_sum/solution.py
@@ -2,20 +2,13 @@ def subarraySum(nums, k):
     """
     Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
     """
-    l = 0
-    r = 0
-    tot = 0
-    while len(nums)-1 >= r >= l:
-        if sum(nums[l: r + 1]) == k:
-            tot += 1
-            r += 1
-        elif sum(nums[l: r + 1]) > k:
-            l += 1
-        else:
-            r += 1
-        if r == len(nums) - 1:
-            l += 1
-    return tot
+    count = 0
+    s = 0
+    for i in range(len(nums)):
+        s += nums[i]
+        if s % k == 0:
+            count += 1
+    return count
 
 
 nums = [1, 2, 3]
