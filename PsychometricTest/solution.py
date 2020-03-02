8a9a42228d909f5a7d62fded4cd34cfdba6905a9
ahmedmeshref <a.meshref@alustudent.com>
2020-03-03 00:35:29 +0200

    soluiotn for scores testing added

--- PsychometricTest/solution.py
+++ PsychometricTest/solution.py
@@ -3,17 +3,38 @@
 #        yield len([1 for score in scores if l <= score <= u])
 
 
+# get the index of the first number greater than or equals to the lower
+def findLower(scores, len_score, v):
+    l = 0
+    u = len_score - 1
+    while l <= u:
+        mid = (l + u) // 2
+        if scores[mid] >= v:
+            u = mid - 1
+        else:
+            l = mid + 1
+    return l
+
+
+# get the index of the last element that is less than or equal to v
+def findUpper(scores, len_score, v):
+    l = 0
+    u = len_score - 1
+    while l <= u:
+        mid = (l + u) // 2
+        if scores[mid] > v:
+            u = mid - 1
+        else:
+            l = mid + 1
+    return u
+
+
 def jobOffers(scores, lowerLimits, upperLimits):
     scores.sort()
-    for l, u in zip(lowerLimits, upperLimits):
-        k = 0
-        r = len(scores)
-        while (scores[k] < l) or (scores[r - 1] > u):
-            if scores[k] < l:
-                k += 1
-            if scores[r - 1] > u:
-                r -= 1
-        return r - k
+    n = len(scores)
+    for index in range(len(lowerLimits)):
+        yield findUpper(scores, n, upperLimits[index]) - findLower(scores, n, lowerLimits[index]) + 1
 
 
-print(jobOffers([1, 2, 2, 2, 3], [1], [5]))
+for i in jobOffers([1, 2, 2, 3, 5, 6, 7], [1], [7]):
+    print(i)
