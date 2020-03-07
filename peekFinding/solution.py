effa1fd3795a4e42b6d7068bac3087f46b09e364
ahmedmeshref <a.meshref@alustudent.com>
2020-03-07 20:05:41 +0200

    new files added

---
+++ peekFinding/solution.py
@@ -0,0 +1,11 @@
+def peek(m):
+    """
+    peek takes in a n*m matrix and get the peek element from it
+    """
+    for i in range(m):
+        
+
+m = [[1, 2, 3],
+     [3, 2, 1],
+     [9, 1, 2]]
+print(peek(m))
