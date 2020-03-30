97514c95cd546d7785f5a65b23e7faf8fd01454a
ahmedmeshref <a.meshref@alustudent.com>
2020-03-30 23:31:30 +0200

    longest string problem solved

--- Lexicographical_Maximum_String/solution.py
+++ Lexicographical_Maximum_String/solution.py
@@ -2,17 +2,26 @@ from collections import Counter
 
 
 def getLargestString(s, k):
-    chars = Counter(s)
-    keys = sorted(chars.keys(), reverse=True)
-    new_s = ""
-    for i in range(len(keys)):
-        counter = i
-        while chars.get(keys[i]):
-            if new_s[-1] != keys[i]:
-                new_s += keys[i] * k
-            else:
-                new_s += keys[counter] * k
-    return new_s
+    d = Counter(s)
+    ord_s = sorted(d.keys(), reverse=True)
+    out = ""
+    l_p = 0
+    while l_p < len(ord_s):
+        if d[ord_s[l_p]] < k:
+            out += ord_s[l_p] * d[ord_s[l_p]]
+            l_p += 1
+            continue
+        else:
+            out += ord_s[l_p] * k
+            d[ord_s[l_p]] -= k
+            if d[ord_s[l_p]] > 0:
+                if len(ord_s) - l_p > 0 and d[ord_s[l_p + 1]] > 0:
+                    out += ord_s[l_p + 1]
+                    d[ord_s[l_p + 1]] -= 1
+                else:
+                    break
+    return out
 
 
-print(getLargestString("yuzzvuyzpv", 1))
+s = "zzzzzzzzyy"
+print(getLargestString(s, 4))
