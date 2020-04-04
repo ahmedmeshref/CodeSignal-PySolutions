e5165be055cbcd91356a4846ba714efb2f479beb
ahmedmeshref <a.meshref@alustudent.com>
2020-04-04 17:38:49 +0200

    Google code Jam challenge 1 solved

--- leetCode_30DaysOfCode/day_4/max_product_sum.py
+++ leetCode_30DaysOfCode/day_4/max_product_sum.py
@@ -1,23 +1,13 @@
 def maxProduct(nums):
-    s = 1
-    negative = 1
-    for num in nums:
-        if num < 0:
-            negative += 1
-        s *= num
-    if negative % 2 == 0:
-        return s
-
-    else:
-        max_so_far = nums[0]
-        curr_max = max_so_far
-        for i in range(1, len(nums)):
-            curr_max *= nums[i]
-            if curr_max < nums[i]:
-                curr_max = nums[i]
-            if curr_max > max_so_far:
-                max_so_far = curr_max
-        return max_so_far
+    max_so_far = nums[0]
+    curr_max = max_so_far
+    for i in range(1, len(nums)):
+        curr_max *= nums[i]
+        if curr_max < nums[i]:
+            curr_max = nums[i]
+        if curr_max > max_so_far:
+            max_so_far = curr_max
+    return max_so_far
 
 
-print(maxProduct([0, 2]))
+print(maxProduct([2, -10, -2, -4, 3]))
