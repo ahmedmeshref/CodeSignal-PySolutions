785c5597902394781dd0ab3b095fc2573d24137f
ahmedmeshref <a.meshref@alustudent.com>
2020-04-12 14:17:30 +0200

    Add comments to the challenge solution

--- leetCode_30DaysOfCode/day_12/last_stone_weight.py
+++ leetCode_30DaysOfCode/day_12/last_stone_weight.py
@@ -11,16 +11,21 @@ At the end, there is at most 1 stone left.  Return the weight of this stone (or 
 
 def lastStoneWeight(stones):
     def getLast(arr):
+        # get the index of the max num
         ind = arr.index(max(arr))
+        # swap the max with the last element to make the deletion process efficient (O(n) time and O(1) space)
         arr[-1], arr[ind] = arr[ind], arr[-1]
         return arr.pop()
 
     while len(stones) > 1:
         # delete the largest ele from the stones list
         largest = getLast(stones)
+        # delete the second largest ele from the stones list
         sec_largest = getLast(stones)
-        if largest > sec_largest:
+        # in case if the largest is not equal to the second largest we add the difference to the stones list again
+        if largest != sec_largest:
             stones.append(largest - sec_largest)
+    # if stones is not empty
     if stones:
         return stones[0]
     return 0
