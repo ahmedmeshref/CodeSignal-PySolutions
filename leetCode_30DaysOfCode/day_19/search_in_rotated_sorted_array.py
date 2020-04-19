f1d5a77a239af76ff015374179f7b661886591e9
ahmedmeshref <a.meshref@alustudent.com>
2020-04-19 13:39:46 +0200

    Better solution with one function only added

--- leetCode_30DaysOfCode/day_19/search_in_rotated_sorted_array.py
+++ leetCode_30DaysOfCode/day_19/search_in_rotated_sorted_array.py
@@ -1,51 +1,68 @@
 """
-Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+Suppose an numsay sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
 (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
 
-You are given a target value to search. If found in the array return its index, otherwise return -1.
+You are given a target value to search. If found in the numsay return its index, otherwise return -1.
 
-You may assume no duplicate exists in the array.
+You may assume no duplicate exists in the numsay.
 
 Your algorithm's runtime complexity must be in the order of O(log n).
 """
 
+# def main(nums, target):
+#     if not nums:
+#         return -1
+#     l, r = two_sorted(nums)
+#     ind = binary_search(nums, 0, l, target)
+#     if ind == -1:
+#         ind = binary_search(nums, r + 1, len(nums) - 1, target)
+#         if ind == -1:
+#             return -1
+#     return ind
+#
+# def two_sorted(nums):
+#     l = 0
+#     r = len(nums) - 1
+#     while l < r:
+#         mid = (l + r) // 2
+#         if nums[mid] > nums[l]:
+#             l = mid
+#         else:
+#             r = mid
+#     return (l, r)
+#
+#
+# def binary_search(nums, l, r, target):
+#     while l <= r:
+#         mid = (l + r) // 2
+#         if nums[mid] == target:
+#             return mid
+#         elif nums[mid] > target:
+#             r = mid - 1
+#         else:
+#             l = mid + 1
+#     return -1
+
+
 def main(nums, target):
-    if not nums:
-        return -1
-    l, r = two_sorted(nums)
-    ind = binary_search(nums, 0, l, target)
-    if ind == -1:
-        ind = binary_search(nums, r + 1, len(nums) - 1, target)
-        if ind == -1:
-            return -1
-    return ind
-
-def two_sorted(arr):
     l = 0
-    r = len(arr) - 1
-    while l < r:
-        mid = (l + r) // 2
-        if arr[mid] > arr[l]:
-            l = mid
-        else:
-            r = mid
-    return (l, r)
-
-
-def binary_search(arr, l, r, target):
+    r = len(nums) - 1
     while l <= r:
         mid = (l + r) // 2
-        if arr[mid] == target:
+        if nums[mid] == target:
             return mid
-        elif arr[mid] > target:
-            r = mid - 1
+        if nums[l] <= nums[mid]:
+            if nums[l] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
         else:
-            l = mid + 1
+            if nums[mid] < target <= nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
     return -1
 
 
-
-
-
-print(main([7, 1, 2, 3, 4, 5, 6], 6))
+print(main([1, 3], 3))
